da772a082ef5584047cf74f8438b2b96ed36eb52
J-9O <jeremy@9thorbit.io>
2023-05-07 20:49:12 -0500

    Reqwest Mod Examples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use reqwest::header::{ACCEPT, CONTENT_TYPE};
+use serde_json;
+
 mod modules;
-use crate::modules::hello;
-use crate::modules::cli_tools::cli::{Cli, CliError};
+//use crate::modules::hello;
+//use crate::modules::cli_tools::cli::{Cli, CliError};
 use crate::modules::reqwest_mod::ReqwestMod;
 
 #[tokio::main]
@@ -8,11 +11,21 @@ async fn main() {
     //hello::hello();
     //let args: Cli = Cli::parse();
     //Cli::run()
-    let result = ReqwestMod::run().await;
+
+    let rm = ReqwestMod::new();
+    let response = rm.get_iced_recipes().await;
+    //let res_vec = response.as_array();
+    for val in response.as_array().unwrap() {
+        println!("{:?}\n", val);
+    }
+    
+    //println!("Success! JSON Received:\n {:?}", response[0]);
+ 
+    /* let result = ReqwestMod::run().await;
     println!("Result: {:?}", result);
     
     let status_code = ReqwestMod::status_code(&result);
-    println!("Status Code: {:?}", status_code);
+    println!("Status Code: {:?}", status_code); */
 }
 
 #[test]
--- src/modules/reqwest_mod/mod.rs
+++ src/modules/reqwest_mod/mod.rs
@@ -1,10 +1,49 @@
 use reqwest;
-
-pub struct ReqwestMod;
+use reqwest::header::{ACCEPT, CONTENT_TYPE};
+use serde_json::{self, error};
+pub struct ReqwestMod {
+  pub client: reqwest::Client
+}
 
 impl ReqwestMod {
+  pub fn new() -> ReqwestMod {
+    ReqwestMod { client: reqwest::Client::new() }
+  }
+
+  pub async fn get_iced_recipes(&self) -> serde_json::Value {
+    let res = &self.client
+        .get("https://api.sampleapis.com/coffee/iced")
+        .header(ACCEPT, "application/json")
+        .header(CONTENT_TYPE, "application/json")
+        .send()
+        .await
+        .unwrap()
+        .json::<serde_json::Value>()
+        .await;
+
+
+    match res {
+        Ok(json_res) => json_res.to_owned(),
+        Err(e) => Self::err_json(e),
+    }
+  }
+
+  fn err_json (e: &reqwest::Error) -> serde_json::Value {
+    let error_json = format!("
+      {{
+        \"Message\": \"UH-OH! We got an error!\", 
+        \"Error\": {:?}
+      }}", 
+      e.to_string()
+    );
+
+    let err = error_json.as_str();
+
+    serde_json::from_str(err).unwrap()
+  }
+
   pub async fn run() -> reqwest::Response {
-    match reqwest::get("https://api.spotify.com/v1/search").await {
+    match reqwest::get("https://api.sampleapis.com/coffee/iced").await {
       Ok(response) => response,
       Err(err) => panic!("Error: {:?}", err)
     }
