b670a32c021cec96ca208162302399814ce25cf8
J-9O <jeremy@9thorbit.io>
2023-05-08 23:36:36 -0500

    Reqwest module demo complete

--- src/modules/reqwest_mod/mod.rs
+++ src/modules/reqwest_mod/mod.rs
@@ -1,6 +1,6 @@
 use reqwest;
 use reqwest::header::{ACCEPT, CONTENT_TYPE};
-use serde_json::{self, error};
+use serde_json::{self};
 pub struct ReqwestMod {
   pub client: reqwest::Client
 }
@@ -10,22 +10,33 @@ impl ReqwestMod {
     ReqwestMod { client: reqwest::Client::new() }
   }
 
-  pub async fn get_iced_recipes(&self) -> serde_json::Value {
+  async fn get_sample_json(&self, url: &str) -> serde_json::Value {
     let res = &self.client
-        .get("https://api.sampleapis.com/coffee/iced")
-        .header(ACCEPT, "application/json")
-        .header(CONTENT_TYPE, "application/json")
-        .send()
-        .await
-        .unwrap()
-        .json::<serde_json::Value>()
-        .await;
-
+      .get(url)
+      .header(ACCEPT, "application/json")
+      .header(CONTENT_TYPE, "application/json")
+      .send()
+      .await
+      .unwrap()
+      .json::<serde_json::Value>()
+      .await;
 
     match res {
-        Ok(json_res) => json_res.to_owned(),
-        Err(e) => Self::err_json(e),
+      Ok(json_res) => json_res.to_owned(),
+      Err(e) => Self::err_json(e),
     }
+  }
+
+  pub async fn get_iced_recipes(&self) -> serde_json::Value {
+    Self::get_sample_json(&self, "https://api.sampleapis.com/coffee/iced").await
+  }
+
+  pub async fn get_hot_recipes(&self) -> serde_json::Value {
+    Self::get_sample_json(&self, "https://api.sampleapis.com/coffee/hot").await
+  }
+
+  pub async fn get_bank_info(&self) -> serde_json::Value {
+    Self::get_sample_json(&self,"https://api.sampleapis.com/fakebank/accounts").await
   }
 
   fn err_json (e: &reqwest::Error) -> serde_json::Value {
@@ -41,15 +52,19 @@ impl ReqwestMod {
 
     serde_json::from_str(err).unwrap()
   }
+  /** Demo function that returns the first JSON object of each API request. */
+  pub async fn run(&self) {
+    // Run all 3 routes, and return the first `Object` from each JSON body
+    let iced_recipes = Self::get_iced_recipes(&self).await;
+    let hot_recipes = Self::get_hot_recipes(&self).await;
+    let bank_info = Self::get_bank_info(&self).await;
 
-  pub async fn run() -> reqwest::Response {
-    match reqwest::get("https://api.sampleapis.com/coffee/iced").await {
-      Ok(response) => response,
-      Err(err) => panic!("Error: {:?}", err)
-    }
+    println!("{}\n", iced_recipes[0]);
+    println!("{}\n", hot_recipes[0]);
+    println!("{}\n", bank_info[0]);
   }
 
-  pub fn status_code(res: &reqwest::Response) -> reqwest::StatusCode {
+  fn status_code(res: &reqwest::Response) -> reqwest::StatusCode {
     res.status() 
   }
 }
